5dcd67b6094690959e588691adaf7c2f6924797b
SylphidZoul <zoulxhikaru@gmail.com>
2021-01-16 16:24:39 -0300

    Project Initialization

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const usersRouter = require('./routes/users')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/users', usersRouter)
+
+module.exports = app
